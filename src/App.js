293821d9dffb954d0727dc706abc61428e0501fa
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-06-01 12:24:21 +0100

    adding useInterval to fetch messages data in every defined seconds

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SignIn from "./SignIn";
+import useInterval from "./useInterval"
 function App() {
   const [data, setData] = useState("");
   const [isUpdatingData, setIsUpdatingData] = useState(false);
@@ -22,6 +23,37 @@ function App() {
       });
   }, [isUpdatingData]);
 
+    useInterval(
+      () => {
+        // Your custom logic here
+          fetch("https://muratdemirtas-chat-server.glitch.me/messages")
+            .then((resp) => {
+              return resp.json();
+            })
+            .then((data) => {
+              setData(data);
+              // setIsUpdatingData(false);
+            })
+            .catch((e) => console.error(e.message));
+        // setCount(count + 1);
+      },
+      // Delay in milliseconds or null to stop it
+     30000    );
+
+
+  // const fetchDataInEvery10 = setInterval(timer, 10000);
+  // function timer() {
+  //   fetch("https://muratdemirtas-chat-server.glitch.me/messages")
+  //     .then((resp) => {
+  //       return resp.json();
+  //     })
+  //     .then((data) => {
+  //       setData(data);
+  //       // setIsUpdatingData(false);
+  //     })
+  //     .catch((e) => console.error(e.message));
+  //   clearInterval(fetchDataInEvery10);
+  // }
   function signInHandler() {
     setIsUserSignedIn(true);
   }
